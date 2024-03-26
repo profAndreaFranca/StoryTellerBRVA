5c7f742183006f034c8621491d3b1eb1bebc7f9b
profAndreaFranca <dejandreafranca@gmail.com>
2024-03-25 21:29:52 -0300

    Aula 95 Final funcionando

--- screens/Home.js
+++ screens/Home.js
@@ -1,12 +1,95 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+  Image,
+} from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
 
-export default class Home extends React.Component{
-    render(){
-        return(
-            <View>
-                <Text>Home</Text>
-            </View>
-        )
+import AppLoading from "expo-app-loading";
+import * as Font from "expo-font";
+import { FlatList } from "react-native-gesture-handler";
+
+import * as SplashScreen from "expo-splash-screen"
+SplashScreen.preventAutoHideAsync()
+
+let customFonts = {
+  "Bubblegum-Sans": require("../assets/fonts/BubblegumSans-Regular.ttf"),
+};
+
+export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fontsLoaded: false,
+    };
+  }
+
+  async _loadFontsAsync() {
+    await Font.loadAsync(customFonts);
+    this.setState({ fontsLoaded: true });
+  }
+
+  componentDidMount() {
+    this._loadFontsAsync();
+  }
+
+  render() {
+    if (this.state.fontsLoaded) {
+      SplashScreen.hideAsync()
+      return (
+        <View style={styles.container}>
+          <View style={styles.appTitleTextContainer}>
+            <Text style={styles.appTitleText}>
+              Bem-Vindo ao App Narração de Histórias
+            </Text>
+          </View>
+          <View style={styles.cardContainer}>
+            <Image
+              source={require("../assets/story_image_1.png")}
+              style={styles.storyImage}
+            ></Image>
+          </View>
+          <View style={styles.textContainer}>
+            <Text>Observação: Utilize a navegação para ler as histórias</Text>
+          </View>
+
+          <View style={{ flex: 0.08 }} />
+        </View>
+      );
     }
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#15193c",
+  },
+  appTitleTextContainer: {
+    flex: 0.7,
+    justifyContent: "center",
+    textAlign: "center",
+  },
+  appTitleText: {
+    color: "white",
+    fontSize: RFValue(28),
+    fontFamily: "Bubblegum-Sans",
+  },
+  storyImage: {
+    width: "90%",
+    height: RFValue(250),
+    alignSelf: "center",
+  },
+  textContainer: {
+    paddingTop: RFValue(20),
+    color: "white",
+    fontSize: RFValue(28),
+    fontFamily: "Bubblegum-Sans",
+    textAlign: "center",
+  },
+});
