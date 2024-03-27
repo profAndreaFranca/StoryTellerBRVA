0031230cedbc8db7f59dffcbe421c4fd4e9ad2ed
profAndreaFranca <dejandreafranca@gmail.com>
2024-03-27 20:27:59 -0300

    temp Stories
    Co-authored-by: AlexandreGamboa <AlexandreGamboa@users.noreply.github.com>
    Co-authored-by: JPSouza12 <JPSouza12@users.noreply.github.com>

---
+++ screens/StoryCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+export default class StoryCard extends React.Component{
+    render(){
+        return(
+            <View>
+                <Text>Story Card</Text>
+            </View>
+        )
+    }
+}
